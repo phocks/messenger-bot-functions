6f454e099885a31d1184724a7bc4ccd23be89e11
Joshua Byrd <phocks@gmail.com>
2019-08-30 16:31:29 +1000

    Fonts etc

--- routes/d3test.js
+++ routes/d3test.js
@@ -5,9 +5,10 @@ const d3 = Object.assign({}, require("d3-selection"), require("d3-scale"));
 
 var data = [1, 2, 13, 20, 23];
 
-var scale = d3.scaleLinear()
+var scale = d3
+  .scaleLinear()
   .range([10, 390])
-  .domain([1,23]);
+  .domain([1, 23]);
 
 module.exports = (req, res) => {
   const img = PImage.make(400, 300);
@@ -17,26 +18,36 @@ module.exports = (req, res) => {
 
   const context = img.getContext("2d");
 
-  // ctx.fillStyle = chroma.random().hex();
-  // ctx.beginPath();
-  // ctx.arc(50, 50, 40, 0, Math.PI * 2, true); // Outer circle
-  // ctx.closePath();
-  // ctx.fill();
+  var fnt = PImage.registerFont("./Roboto-Regular.ttf", "Roboto");
 
-  data.forEach(function(d, i) {
-    context.beginPath();
-    context.fillStyle="red";
-    context.fillRect(scale(d), 150, 10, 10);
-    context.fill();
-    context.closePath();
-  });
+  fnt.load(() => {
+    console.log("loaded");
 
-  PImage.encodePNGToStream(img, fs.createWriteStream("/tmp/out.png"))
-    .then(() => {
-      const src = fs.createReadStream("/tmp/out.png");
-      src.pipe(res);
-    })
-    .catch(e => {
-      console.log("there was an error writing");
+    context.fillStyle = "green";
+    context.font = "48pt 'Roboto'";
+    context.fillText("ABC", 80, 80);
+
+    data.forEach(function(d, i) {
+      context.beginPath();
+      context.fillStyle = "red";
+      context.fillRect(scale(d), 150, 10, 10);
+      context.fill();
+      context.closePath();
     });
+
+    context.fillStyle = chroma.random().hex();
+    context.beginPath();
+    context.arc(50, 50, 40, 0, Math.PI * 2, true); // Outer circle
+    context.closePath();
+    context.fill();
+
+    PImage.encodePNGToStream(img, fs.createWriteStream("/tmp/out.png"))
+      .then(() => {
+        const src = fs.createReadStream("/tmp/out.png");
+        src.pipe(res);
+      })
+      .catch(e => {
+        console.log("there was an error writing");
+      });
+  });
 };
--- routes/nameHistoryInteractive.js
+++ routes/nameHistoryInteractive.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
           type: "image",
           payload: {
             url:
-              "https://us-central1-abc-news-169508.cloudfunctions.net/messenger-bot-functions/circle-random-color?" +
+              "https://us-central1-abc-news-169508.cloudfunctions.net/messenger-bot-functions/d3test?" +
               uuidv1()
           }
         }
