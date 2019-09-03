d688c3483d829915ed69eb031b77ef6f60b0294f
Joshua Byrd <phocks@gmail.com>
2019-09-03 12:30:00 +1000

    Add nodemon

--- routes/chart-js-test.js
+++ routes/chart-js-test.js
@@ -19,7 +19,7 @@ module.exports = (req, res) => {
         {
           label: "My First dataset",
           backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(200, 99, 132)",
           data: [0, 10, 5, 2, 20, 30, 45]
         }
       ]
