0ce1dfbb8d8cfe359c510731f338b8ec1a5d4a8c
Joshua Byrd <phocks@gmail.com>
2019-09-04 15:21:47 +1000

    Removing dist

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res, next) => {
-    res.json({
-        message: "Welcome to the messenger bot API. Written by Joshua Byrd. Please have a nice day."
-    });
-};
-//# sourceMappingURL=index.js.map
