2982cf0e23a306ee6254bcd2792943a2f9dcad41
Joshua Byrd <phocks@gmail.com>
2019-10-09 16:01:29 +1000

    messages dynamic

--- src/routes/cloudBot.ts
+++ src/routes/cloudBot.ts
@@ -1,13 +1,20 @@
+const prompts = [
+  "How's it going?",
+  "How has your day been so far?",
+  "What does the sky look like right now?",
+  "have you remembered to drink some water today?"
+];
+
 export default (req, res) => {
   console.log(req.body);
 
-  res.json({
-    set_attributes: {
-      "some attribute": "some value",
-      "another attribute": "another value"
-    },
-    block_names: ["Block 1"],
-    type: "show_block",
-    title: "Go!"
-  });
+  const payload = {
+    messages: [
+      {
+        text: prompts[Math.ceil(Math.random() * prompts.length)]
+      }
+    ]
+  };
+
+  res.json(payload);
 };
