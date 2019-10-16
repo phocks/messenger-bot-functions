7f882b5598ac9f0f0b09a8b25d935dc057950652
Joshua Byrd <phocks@gmail.com>
2019-10-16 15:07:50 +1000

    Testing different phrases

--- src/routes/cloudBot.ts
+++ src/routes/cloudBot.ts
@@ -2,7 +2,8 @@ const prompts = [
   "How's it going?",
   "How has your day been so far?",
   "What does the sky look like right now?",
-  "have you remembered to drink some water today?"
+  "Have you remembered to drink some water today?",
+  "Just what do you think you're doing Dave?"
 ];
 
 export default (req, res) => {
