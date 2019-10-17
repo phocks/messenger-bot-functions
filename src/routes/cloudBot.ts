3d5b958869d74c737d01eb19303d2376fb0f4e78
Joshua Byrd <phocks@gmail.com>
2019-10-17 13:26:21 +1000

    Goodbye

--- src/routes/cloudBot.ts
+++ src/routes/cloudBot.ts
@@ -4,17 +4,35 @@ elizabot.start(); // initializes eliza and returns a greeting message
 
 // let reply = elizabot.reply("I am tired") // returns a eliza-like reply based on the message text passed into it
 
-const prompts = [
-  "How's it going?",
-  "How has your day been so far?",
-  "What does the sky look like right now?",
-  "Have you remembered to drink some water today?",
-  "Just what do you think you're doing Dave?"
-];
+// const prompts = [
+//   "How's it going?",
+//   "How has your day been so far?",
+//   "What does the sky look like right now?",
+//   "Have you remembered to drink some water today?",
+//   "Just what do you think you're doing Dave?"
+// ];
 
 export default (req, res) => {
   console.log(req.body);
-  let reply = elizabot.reply(req.body["user response"]);
+
+  const userResponse = req.body["user response"] || "";
+
+  // Escape the loop
+  if (userResponse.toLowerCase() === "bye") {
+    const payload = {
+      messages: [
+        {
+          // text: prompts[Math.ceil(Math.random() * prompts.length - 1)]
+          text: "Goodbye."
+        }
+      ]
+    };
+
+    res.json(payload);
+    return;
+  }
+
+  let reply = elizabot.reply(userResponse);
   console.log(reply);
 
   const payload = {
@@ -24,7 +42,7 @@ export default (req, res) => {
         text: reply
       }
     ],
-    redirect_to_blocks: [req.body["last visited block name"]]
+    redirect_to_blocks: [req.body["Redirect block name"]]
   };
 
   res.json(payload);
